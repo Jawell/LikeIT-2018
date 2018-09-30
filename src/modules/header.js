6c5e4dcbd31347ab70c5f8cce3fc40b99865f13c
jawell <skytor08@gmail.com>
2018-09-30 16:19:20 +0300

    add mobile header

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import header from '../assets/header.svg';
-import logo from '../assets/logo.svg'
 
 class Header extends React.Component
 {
     render() {
         return (
             <sector className="header">
-                <div style={backHeaderImg}>
+                <div className="header-background">
                     <div className="wrapper">
-                        <div className="logo" style={logoHeader}/>
+                        <div className="logo"/>
                         <div className="header-info">
                             <p><span className="highlight-text">IT-хакатон в Запорожье</span></p>
                             <p><span className="highlight-text">&#60;</span><span className="header-text">BEST</span><span className="normal-text"> Competition<br/>for </span><span className="header-text">Geeks</span><span className="highlight-text">&#47;&#62;</span></p>
@@ -36,18 +34,5 @@ class Header extends React.Component
         );
     }
 }
-
-let logoHeader = {
-    background: `url(${logo}) no-repeat`,
-    width: "14.5vw",
-    height: "2.74vw"
-};
-
-let backHeaderImg = {
-    background: `url(${header})`,
-    backgroundSize: "100vw",
-    width: "100vw",
-    height: "36.6vw"
-};
 
 export default Header;
