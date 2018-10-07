213c2a41991cc9f7c435aa1e7344de2385115e4b
jawell <skytor08@gmail.com>
2018-10-07 15:07:14 +0300

    fix clip path

--- src/modules/DividerDesktop.js
+++ src/modules/DividerDesktop.js
@@ -8,7 +8,7 @@ class DividerDesktop extends React.Component{
           <clipPath id="third-layer" clipPathUnits="objectBoundingBox">
             <polyline points="0,0.97 0.4884,0.7 0.4730,0.3 1,0 1,0.01 0.4750,0.31 0.4904,0.71 0,0.98"></polyline>
           </clipPath>
-          <rect width="100%" height="100%" clip-path="url(#third-layer)" fill="none" style={polyline}></rect>
+          <rect width="100%" height="100%" clipPath="url(#third-layer)" fill="none" style={polyline}></rect>
         </svg>
       </section>
     )
