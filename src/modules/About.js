56f5524c2b51c3626c2ac61f1ef96db0ecaa9b3b
jawell <skytor08@gmail.com>
2018-10-09 01:45:01 +0300

    fix fonts

--- src/modules/About.js
+++ src/modules/About.js
@@ -26,9 +26,9 @@ class About extends React.Component
               <p className="header-column-mobile"><span className="highlight">что?</span></p>
               <div className="text-info">
                 <p><span className="highlight header-column">что?</span></p>
-                <p>Мероприятие состоит из двух частей: <span className="headerlight">Ярмарки Карьерв</span> и <span className="headerlight">Хакатона</span>.</p>
-                <p><span className="highlight">Ярмарка Карьеры</span> - это 3-часовая презентация ведущих IT-компаний Украины и мира.</p>
-                <p><span className="highlight">Хакатон</span> - «марафон программистов», где небольшие команды специалисты из разных областей разработки программного обеспечения (программисты, дизайнеры, менеджеры) сообща работают над решением какой-либо проблемы.</p>
+                <p>Мероприятие состоит из двух частей: <span className="headerlight bold-inside">Ярмарки Карьерв</span> и <span className="headerlight bold-inside">Хакатона</span>.</p>
+                <p><span className="highlight bold-inside">Ярмарка Карьеры</span> - это 3-часовая презентация ведущих IT-компаний Украины и мира.</p>
+                <p><span className="highlight bold-inside">Хакатон</span> - «марафон программистов», где небольшие команды специалисты из разных областей разработки программного обеспечения (программисты, дизайнеры, менеджеры) сообща работают над решением какой-либо проблемы.</p>
               </div>
             </div>
 
@@ -38,7 +38,7 @@ class About extends React.Component
                 <img src={Calendar} alt={Calendar} className="img-field"/>
                 <div className="short-info-cell">
                   <p><span className="highlight header-column">когда?</span></p>
-                  <p><span className="headerlight">24-25</span> ноября</p>
+                  <p><span className="headerlight bold-inside">24-25</span> ноября</p>
                 </div>
               </div>
 
@@ -61,7 +61,7 @@ class About extends React.Component
                 <img src={Programmer} alt={Programmer} className="img-field"/>
                 <div className="short-info-cell">
                   <p><span className="highlight header-column">количество участников</span></p>
-                  <p><span className="headerlight">100</span> человек</p>
+                  <p><span className="headerlight bold-inside">100</span> человек</p>
                 </div>
               </div>
 
