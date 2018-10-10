c31319a8d301fa4cc543a14eb4f07d96e3d51ae0
jawell <skytor08@gmail.com>
2018-10-10 22:24:30 +0300

    add organizers landscape

--- src/modules/Organizers.js
+++ src/modules/Organizers.js
@@ -10,8 +10,25 @@ class Organizers extends React.Component
           <div className="about-header block-header">
             <p><span>&#62;</span><span> организаторы</span></p>
           </div>
+        </div>
+        <div className="left-wrapper">
           <div id="organizers-info">
-            <img src={OrganizersPhoto} alt={OrganizersPhoto}/>
+            <div id="shadow-img">
+              <img src={OrganizersPhoto} alt={OrganizersPhoto}/>
+            </div>
+            <div>
+              <p>
+                <span className="headerlight">BEST (Board of European Students of Technology)</span> – совет студентов технических ВУЗов Европы –постоянно растущая, некоммерческая и неполитическая организация.
+              </p>
+              <p>
+                Начиная <span className="highlight">с 1989</span> года, эта инновационная студенческая сеть обеспечивает студентов разных специальностей возможностью общения, взаимодействия и культурного
+                обмена, а также предоставляет дополнительное образование в разных сферах
+                человеческих и инженерных знаний.
+              </p>
+              <p>
+                <span className="highlight">Наша цель</span> - налаживание тесной связи между компаниями и студентами! Ежегодно мы собираем мотивированную молодежь разных специальностей, чтобы дать им возможность получить новые знания и раскрыть свой потенциал.
+              </p>
+            </div>
           </div>
         </div>
       </section>
