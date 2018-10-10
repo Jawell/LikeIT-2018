bb3298fdfd14d0ebd801a9dfbe4cb841b55259a0
jawell <skytor08@gmail.com>
2018-10-10 14:41:28 +0300

    refactor code

--- src/modules/DividerPoly.js
+++ src/modules/DividerPoly.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
-class DividerPoly extends React.Component{
-  render(){
-    return (
+class DividerPoly extends React.Component
+{
+  render() {
+    return(
       <section id="divider_1">
         <svg className="poly" width="100%" height="7.135vw">
-          <clipPath id="third-layer" clipPathUnits="objectBoundingBox">
+          <clipPath id="divider-poly" clipPathUnits="objectBoundingBox">
             <polyline points="0,0.97 0.4884,0.7 0.4730,0.3 1,0 1,0.01 0.4750,0.31 0.4904,0.71 0,0.98"></polyline>
           </clipPath>
-          <rect width="100%" height="100%" clipPath="url(#third-layer)" fill="none" style={polyline}></rect>
+          <rect width="100%" height="100%" clipPath="url(#divider-poly)" fill="none" style={polyline}></rect>
         </svg>
       </section>
     )
   }
 }
 
-let polyline = {
+const polyline = {
   stroke: "#fbab18",
   strokeWidth: "7.135vw",
 };
